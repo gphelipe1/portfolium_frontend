ffffb1f4ce71dcd2d6e9373db14dda845c2c8d46
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2020-12-05 22:11:08 -0300

    feature: add card component

---
+++ frontend/src/components/Card/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Card = ({ children }) => <Container>{children}</Container>;
+
+export default Card;
---
+++ frontend/src/components/Card/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 12px;
+  background-color: #ffffff;
+  border: 1px solid #e5e5e5;
+  border-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+  align-items: center;
+  justify-content: center;
+`;
