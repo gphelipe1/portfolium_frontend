9df4670824775744232a2681f5dc88357cda9282
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2020-12-06 06:37:09 -0300

    fix: show login button only when user isn't logged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const ContainerWithStore = withStore(Container);
 
   const renderUserInfo = () => {
-    return userId ? (
+    return !userId ? (
       <Link to="/login">
         <Button>Login</Button>
       </Link>
