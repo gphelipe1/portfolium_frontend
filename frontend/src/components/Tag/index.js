d57de42a993ae34740fb0d3e0e3c9f86178f7af6
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2020-12-06 02:18:09 -0300

    feature: add tag component

---
+++ frontend/src/components/Tag/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Tag = ({ children }) => <Container>{children}</Container>;
+
+export default Tag;
---
+++ frontend/src/components/Tag/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3px 10px;
+  color: #526bc1;
+  font-size: 12px;
+  font-weight: bold;
+  border: 1px solid #688dd7;
+  border-radius: 15px;
+`;
