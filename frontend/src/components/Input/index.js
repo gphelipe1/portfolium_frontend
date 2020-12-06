8213825f919e6f9529531e4237f0cc963ed81e61
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2020-12-06 04:05:02 -0300

    feature: add Input component

---
+++ frontend/src/components/Input/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Input = ({ placeholder = "" }) => <Container placeholder={placeholder} />;
+
+export default Input;
---
+++ frontend/src/components/Input/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.input`
+  display: flex;
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #b5b5b5;
+  border-radius: 8px;
+`;
